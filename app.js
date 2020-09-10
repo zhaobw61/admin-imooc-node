e2205a2f46132206c20d06131b18c2e57bfac9e8
zhaobowen <bowen.zhao@chiefclouds.com>
2020-09-10 19:12:39 +0800

    初始化登录接口

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const router = require('./router');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 app.use('/', router);
 
-const server = app.listen(5000, function(){
-    const { address, port } = server.address();
-    console.log('启动成功', address, port);
+const SSLPORT = 18082;
+
+const server = app.listen(SSLPORT, function(){
+    console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT)
+
 });
+
+// const fs = require('fs')
+// const https = require('https')
+
+// const privateKey = fs.readFileSync('https/book_youbaobao_xyz.key', 'utf8')
+// const certificate = fs.readFileSync('https/book_youbaobao_xyz.pem', 'utf8')
+// const credentials = { key: privateKey, cert: certificate }
+// const httpsServer = https.createServer(credentials, app)
+// const SSLPORT = 18082
+// httpsServer.listen(SSLPORT, function() {
+//   console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT)
+// })
--- router/user.js
+++ router/user.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
+router.post('/login', function(req, res){
+    console.log(req.body);
+    res.json({
+        code: 0,
+        msg: '登录成功'
+    });
+});
+
 router.get('/info', function(req, res, next){
     res.json('user info...');
 });
