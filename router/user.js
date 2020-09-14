d3421c19fd991f02763e064e52651495eff3640e
zhaobowen <bowen.zhao@chiefclouds.com>
2020-09-14 17:43:58 +0800

    添加参数验证

--- router/user.js
+++ router/user.js
@@ -3,20 +3,32 @@ const Result = require('../models/Result');
 const { login } = require('../services/user');
 const { md5 } = require('../utils/index');
 const { PWD_SALT } = require('../utils/constant');
+const { body, validationResult } = require('express-validator');
 
 const router = express.Router();
 
-router.post('/login', function(req, res){
-    let { username, password } = req.body;
-
-    password = md5(`${password}${PWD_SALT}`);
-    login(username, password).then(user=>{
-        if(!user || user.length === 0) {
-            new Result('登录失败').fail(res);
+router.post(
+    '/login',
+    [
+        body('username').isString().withMessage('用户名必须为字符'),
+        body('password').isString().withMessage('密码必须为字符'),
+    ],
+    function(req, res, next){
+        const err = validationResult(req);
+        if(!err.isEmpty()) {
+            const [{msg}] = err.errors;
+            next(boom.badRequest(msg));
         } else {
-            new Result('登录成功').success(res);
+            let { username, password } = req.body;
+            password = md5(`${password}${PWD_SALT}`);
+            login(username, password).then(user=>{
+                if(!user || user.length === 0) {
+                    new Result('登录失败').fail(res);
+                } else {
+                    new Result('登录成功').success(res);
+                }
+            })
         }
-    })
 });
 
 router.get('/info', function(req, res, next){
